78e62e92b25beb99d9ad5cf239b12549503615a3
chandu35-hub <chandumaddipatla2@gmail.com>
2024-02-01 13:04:30 +0530

    Connected frontend to backend

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,16 +1,32 @@
-import React from 'react'
-import {Form, Input} from 'antd'
-import { Link } from 'react-router-dom'
+import React, {useState} from 'react'
+import {Form, Input, message} from 'antd'
+import { Link, useNavigate } from 'react-router-dom'
+import axios from 'axios'
+import Spinner from '../components/Spinner'
 
 const Login = () => {
+    const [loading, setLoading] = useState (false)
+    const navigate = useNavigate()
+
     // form Submit
-    const submitHandler = (values) => {
-        console.log(values)
+    const submitHandler = async (values) => {
+        try {
+            setLoading(true)
+            const {data} = await axios.post('/users/login', values)
+            setLoading(false)
+            message.success('login success')
+            localStorage.setItem('user', JSON.stringify({...data, password:''}))
+            navigate('/')
+        } catch (error) {
+            setLoading(false)
+            message.error('something went wrong')
+        }
     }
 
   return (
     <>
       <div className='register-page'>
+        {loading && <Spinner />}
         <Form layout='vertical' onFinish={submitHandler}>
             <h1>Login form</h1>
             <Form.Item label='Email' name='email'>
