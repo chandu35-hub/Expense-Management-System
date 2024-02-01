8fe087cbd8ec6b6524d55e98ce05d766486f6fc4
chandu35-hub <chandumaddipatla2@gmail.com>
2024-02-01 08:08:28 +0530

    Created frontend client repo

---
+++ client/src/components/layouts/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='bg-dark text-light'>
+      <h6 className='text-center'>All rights reserved &copy; Chandu Maddipatla</h6>
+    </div>
+  )
+}
+
+export default Footer
