0e94caa6e66e5ac380d24403cc26200d74e6b902
chandu35-hub <chandumaddipatla2@gmail.com>
2024-02-08 13:19:30 +0530

    Added Modal

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,12 +1,33 @@
-import React from 'react'
-import Layout from '../components/layouts/Layout'
+import React, { useState } from "react";
+import { Modal } from "antd";
+import Layout from "../components/layouts/Layout";
 
 const HomePage = () => {
+  const [showModal, setShowModal] = useState(false);
   return (
     <Layout>
-      <h1>Home Page</h1>
+      <div className="filters">
+        <div>range filters</div>
+        <div>
+          <button
+            className="btn btn-primary"
+            onClick={() => setShowModal(true)}
+          >
+            Add New
+          </button>
+        </div>
+      </div>
+      <div className="content"></div>
+      <Modal
+        title="Add Transaction"
+        visible={showModal}
+        onCancel={() => setShowModal(false)}
+        footer = {false}
+      >
+        <h1>Hello</h1>
+      </Modal>
     </Layout>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
