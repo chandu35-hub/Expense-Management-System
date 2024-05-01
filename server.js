027d3c0fde96cfe3a23f739d8529fb944f286a80
chandu35-hub <chandumaddipatla2@gmail.com>
2024-05-01 06:41:31 +0530

    Created basic server

--- server.js
+++ server.js
@@ -3,31 +3,27 @@ const cors = require ('cors')
 const morgan = require ('morgan')
 const dotenv = require ('dotenv')
 const colors = require ('colors')
-const connectDb = require ('./config/connectDb')
 
-// config dot env file 
+// Config dot env file
 dotenv.config()
-
-// database call
-connectDb()
 
 // rest object
 const app = express()
 
-// middlewares
-app.use(morgan('dev'))
-app.use(express.json())
+// Middlewares
+app.use (morgan('dev'))
+app.use (express.json())
 app.use(cors())
 
-// routes
-// user routes
-app.use('/api/v1/users', require ('./routes/userRoute'))
-// transaction routes
-app.use('/api/v1/transactions', require('./routes/transactionRoutes'))
-// port
+// Routes
+app.get ('/', (req, res) => {
+    res.send ('<h1>Hello from Server</h1>')
+})
+
+// Port
 const PORT = 8080 || process.env.PORT
 
-// listen server
+// Listen Server
 app.listen (PORT, () => {
     console.log (`Server running on port ${PORT}`)
 })
