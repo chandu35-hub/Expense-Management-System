451b0d18ee5ac82659e7760116274a6752c26756
chandu35-hub <chandumaddipatla2@gmail.com>
2024-05-01 06:20:06 +0530

    Restart

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,11 @@
-import {Routes, Route, Navigate} from 'react-router-dom'
-import HomePage from './pages/HomePage';
-import Register from './pages/Register';
-import Login from './pages/Login';
+import React from 'react'
 
-function App() {
+const App = () => {
   return (
-    <>
-      <Routes>
-        <Route path='/' element={
-          <ProtectedRoutes>
-            <HomePage />
-          </ProtectedRoutes>
-        } />
-        <Route path='/register' element={<Register />} />
-        <Route path='/login' element={<Login />} />
-      </Routes>
-    </>
-  );
+    <div>
+      App
+    </div>
+  )
 }
 
-export function ProtectedRoutes(props) {
-  if (localStorage.getItem('user')) {
-    return props.children
-  }
-  else {
-    return <Navigate to='/login' />
-  }
-}
-
-export default App;
+export default App
